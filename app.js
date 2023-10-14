362364e070d173efe03603a28bedb48dcbc2fc0f
saeed <mostafazarei13751375@gmail.com>
2023-10-14 18:25:08 +0330

    calc price

--- app.js
+++ app.js
@@ -5,26 +5,36 @@ const form = document.querySelector("#request-quote");
 // event   ======================================
 
 document.addEventListener("DOMContentLoaded", afterload);
-form.addEventListener("submit", submitForm);
+document.addEventListener("submit", submitForm);
 
 // function   ===================================
-
+function afterload() {
+  displayYears();
+}
 // submit form
 function submitForm(e) {
   e.preventDefault();
 
-  //   read value from form
-  const make = document.querySelector("#make").value;
-  const year = document.querySelector("#year").value;
-  const level = document.querySelector("input[name=level]:checked");
+  // read value from the form
+  const make = document.querySelector('#make').value
+  const year = document.querySelector('#year').value
+  const level = document.querySelector('input[name="level"]:checked').value
 
-  //   Validation
+  // check the value of fileds are correct
   if (make === "" || year === "" || level === "") {
-    console.log("khalei");
-    displayMsg("لطفاً مقادیر فرم را با دقت پر نمایید. با تشکر");
+      displayMsg('لطفاً مقادیر فرم را با دقت پر نمایید. با تشکر')
   } else {
-    console.log("pore");
-    // console.log(insuranceCase(make,year,level.value))
+      // STEP1: get info
+      let insuranceCase = {
+          make: make,
+          year: year,
+          level: level
+      }
+
+      // STEP2: calculate
+      calculatePrice(insuranceCase)
+
+      // STEP3: show result message box
   }
 }
 
@@ -37,62 +47,87 @@ function submitForm(e) {
 // }
 
 // step 1: get info
-let insuranceCase = {
-  make: make,
-  year: year,
-  level: level,
-};
-
-// step2 :calculate
-calculatePrice(insuranceCase);
-
-// step3: show result message box
 
 function calculatePrice(info) {
-  let price = 0,
-    base = 2000000;
-  // calculate make
-  /*
-make:1 => 1.25
-make:2 => 1.30
-make:3 => 1.80
+  let price = 0, base = 2000000;
+// + Calculate Make 
+    /* 
+    make:1      =>      1.15
+    make:2      =>      1.30
+    make:3      =>      1.80
+    */
 
- */
+    const make = info.make
+    switch (make) {
+        case "1":
+            price = base * 1.15
+            break;
+        case "2":
+            price = base * 1.30
+            break;
+        case "3":
+            price = base * 1.80
+            break;
+    }
 
-  const make = info.make;
-  switch (make) {
-    case "1":
-      price = base * 1.15;
-      break;
-    case "2":
-      price = base * 1.3;
-      break;
-    case "3":
-      price = base * 1.8;
-      break;
-  }
-  // calculate year
-  // get the year
-  const year = info.year;
-  // diffrence= getYearDiffrence(year)
 
-  diffrence = function (year) {
-    // Convert to number
-    // get max year
-    const now = new Date().toLocaleDateString("fa-IR");
-    let nowYear = now.slice(0, 4);
-    let max = fixNumbers(nowYear);
-    year = max - year;
-    return year;
-  };
+  
+    // + Calculate Year
+    // get the year
+    const year = info.year
+    // diffrence = getYearDiffrence(year)
+    const diffrence = function (year) {
+        // Convert to number
+        let
+            persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+            arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+            fixNumbers = function (str) {
+                if (typeof str === 'string') {
+                    for (var i = 0; i < 10; i++) {
+                        str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+                    }
+                }
+                return parseInt(str);
+            };
+
+        // get max year
+        const now = new Date().toLocaleDateString('fa-IR')
+        let nowYear = now.slice(0, 4)
+        let max = fixNumbers(nowYear)
+        year = max - year
+
+        return year
+    }
 
   // 3% cheaper for each year
-  price = price - ((diffrence * 3) / 100) * price;
+  price = price - ((diffrence(year) * 3) / 100) * price
+  console.log(price);
+
+
+    // + get the level
+    const level = info.level
+    price = calculateLevel(level , price)
 }
 
-function afterload() {
-  displayYears();
+function calculateLevel(level , price){
+    /*
+        basic   =>  increase 30%
+        complete=>  increase 50%
+    */
+
+    if (level == 'basic'){
+        // price = price + (price * 0.30) (bara mehrdad)
+        price = price * 1.3
+    }else{
+        price = price * 1.5
+    }
+
+    return price
 }
+  
+
+
+
 // user InterFace (UI ) Functions
 // display message box
 function displayMsg(msg) {
