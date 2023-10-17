87853114e32995253331e6178cabe88714ff2ef2
saeed <mostafazarei13751375@gmail.com>
2023-10-17 13:14:56 +0330

    functional displayYears

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 
 const form = document.querySelector("#request-quote");
 
+// Prices and numbers are inside this variable
+const priceNumber= {
+  //Coefficient of pride
+  make1: 1.15,
+  //Coefficient of optima
+  make2: 1.30,
+  //Coefficient of Porsche
+  make3: 1.80,
+  // base price for clalculate Insurance
+  base : 2000000,
+  // price before start proccessing
+  price : 0
+
+}
 // event   ======================================
 
 document.addEventListener("DOMContentLoaded", afterload);
@@ -34,39 +48,26 @@ function submitForm(e) {
       // STEP2: calculate
       calculatePrice(insuranceCase)
 
-      // STEP3: show result message box
+      
   }
 }
 
-// function insuranceCase(inCarMake,inCarYear,inLevel) {
-//   return{
-//     carMake: inCarMake,
-//     carYear: inCarYear,
-//     carLevel: inLevel,
-//   }
-// }
-
-// step 1: get info
 
 function calculatePrice(info) {
-  let price = 0, base = 2000000;
+  let 
 // + Calculate Make 
-    /* 
-    make:1      =>      1.15
-    make:2      =>      1.30
-    make:3      =>      1.80
-    */
+  
 
     const make = info.make
     switch (make) {
         case "1":
-            price = base * 1.15
+            price = base * priceNumber.make1
             break;
         case "2":
-            price = base * 1.30
+            price = base * priceNumber.make2
             break;
         case "3":
-            price = base * 1.80
+            price = base * priceNumber.make3
             break;
     }
 
@@ -124,22 +125,31 @@ function calculateLevel(level , price){
 
     return price
 }
-  
+
 
 
 
 // user InterFace (UI ) Functions
 // display message box
 function displayMsg(msg) {
+
+
   // create message box
   const messageBox = document.createElement("div");
   messageBox.classList = "error";
   messageBox.innerText = msg;
 
-  //show message
-  form.insertBefore(messageBox, document.querySelector(".form-group"));
+  // this function use for where error message show  
+  insertBeforeorAfter(form,messageBox,".form-group")
 
+  // get time of disapear and in the element that we want to disapear 
   timeOutError(2000, ".error");
+}
+
+// get element and the class of it and show where our message displayed
+function insertBeforeorAfter(kindOfElement,beforeOrAfterElment,selectelement){
+  kindOfElement.insertBefore(beforeOrAfterElment, document.querySelector(selectelement));
+
 }
 
 // get time & element to remove element in that time
@@ -152,30 +162,39 @@ function timeOutError(time, element) {
 
 // display years in optionsTage
 function displayYears() {
+
   // get now date
   let curentYears = new Date().toLocaleDateString("fa-IR");
-
   // slice the year
   curentYears = curentYears.slice(0, 4);
   // max year
   let maxYear = fixNumbers(curentYears);
   const selectYears = document.querySelector("#year");
 
-  const optionTag = document.createElement("option");
-  optionTag.innerText = `   انتخاب`;
 
-  optionTag.value = "";
+ // craet options for kind of car  that coustemer should select
+  createOption (selectYears, `   انتخاب`,"",false)
 
-  selectYears.appendChild(optionTag);
   // mine year
   let minYear = maxYear - 20;
-  for (let i = maxYear; i >= minYear; i--) {
-    const optionTag = document.createElement("option");
-    optionTag.value = i;
 
-    optionTag.innerText = `سال ${i}`;
-    selectYears.appendChild(optionTag);
+
+  for (let i = maxYear; i >= minYear; i--) {
+    // craet options for years that coustemer should select
+    createOption (selectYears, `سال ${i}`,i,false)
   }
+}
+
+
+// make options get text value and where append the element 
+function createOption (selectelement, text,value,seleced){
+  const optionTag = document.createElement("option")
+  optionTag.innerText=text
+  optionTag.value=value 
+  if (seleced){
+    optionTag.selected=true;
+  }
+  selectelement.appendChild(optionTag)
 }
 
 // give us the years in persian number 
